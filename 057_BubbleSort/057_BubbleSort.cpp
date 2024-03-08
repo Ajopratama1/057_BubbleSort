183b162f7fee5e98d562f22acf4de74c6ad368ff
Ajopratama1 <a.ajopratama01@gmail.com>
2024-03-08 16:40:24 +0700

    prosedur display

--- 057_BubbleSort/057_BubbleSort.cpp
+++ 057_BubbleSort/057_BubbleSort.cpp
@@ -54,6 +54,29 @@ void bubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubbl
 
 }
 
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        if (j < n - 1) {
+            cout << " -->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass 
+    cout << endl;
+    cout << endl;
+
+}
+
+
+
+
 
 
 
